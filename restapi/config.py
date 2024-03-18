52514d04ace51aade7f739651f067d68d7a6a262
Rodion <rodbel229@gmail.com>
2024-03-18 18:46:08 +0500

    Start project

---
+++ restapi/config.py
@@ -0,0 +1,15 @@
+import environ
+
+env = environ.Env(
+    DEBUG=(bool, False)
+)
+
+environ.Env.read_env()
+
+SECRET_KEY = env.get_value('SECRET_KEY')
+
+DB_NAME = env.get_value("DB_NAME")
+DB_HOST = env.get_value("DB_HOST")
+DB_PORT = env.get_value("DB_PORT")
+DB_USER = env.get_value("DB_USER")
+DB_PASS = env.get_value("DB_PASS")
---
+++ restapi/restapi/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path, include
+from .swagger import urlpatterns as swagger_urls
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/v1/', include(swagger_urls))
+]
+
